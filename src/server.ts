d4e423d292f79d66c007895d618bd65e83961e4a
ebinmoolan@gmail.com <bennye@tcd.ie>
2019-04-04 21:51:32 +0100

    Fixed bug with encryptedSessions

--- src/server.ts
+++ src/server.ts
@@ -39,7 +39,7 @@ router.get('/getEncryptedSession', cors(), (req, res) => {
     pubKey,
     data => {
       return res.json({
-        data,
+        session: data,
         success: true,
       });
     },
@@ -129,5 +129,6 @@ router.get('/getGroups', cors(), (req, res) => {
 });
 
 app.use('/api', router);
+app.disable('etag');
 
 app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
